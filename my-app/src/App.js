3e9c20507dbce19add64d799f90fcadb99f3c6dc
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-06-24 08:42:49 +0000

    leard about hooks in react

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UseEffectCounterContainer from './components/UseEffectCounterContainer';
+
+function App() {
+  return (
+    <div className="App">
+     <UseEffectCounterContainer />
+    </div>
+  );
+}
+
+export default App;
